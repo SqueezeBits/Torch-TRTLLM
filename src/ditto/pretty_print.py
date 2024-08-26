795fb5c906e611a6cca769583c5bb6bdfbf4e172
Jiwoong Choi <jiwoong.choi@squeezebits.com>
2024-08-26 17:58:41 +0900

    chore: print device of a tensor

--- src/ditto/pretty_print.py
+++ src/ditto/pretty_print.py
@@ -8,7 +8,7 @@ import torch
 def brief_tensor_repr() -> Generator[None, None, None]:
     def tensor_repr(self: torch.Tensor, *, tensor_contents=None) -> str:
         dtype_repr = f"{self.dtype}".removeprefix("torch.")
-        return f"Tensor(shape={tuple(self.shape)}, dtype={dtype_repr})"
+        return f"Tensor(shape={tuple(self.shape)}, dtype={dtype_repr}, device={self.device})"
 
     original_tensor__repr__ = torch.Tensor.__repr__
     torch.Tensor.__repr__ = tensor_repr
