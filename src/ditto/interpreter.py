5378a7f42ab205dde762b7e23562dd11e9055ac4
Jiwoong Choi <jiwoong.choi@squeezebits.com>
2024-11-29 17:34:16 +0900

    fix: `_populate_trt_builder_config` now uses user-defined config

--- src/ditto/interpreter.py
+++ src/ditto/interpreter.py
@@ -1,18 +1,15 @@
 import json
-import os
-from pathlib import Path
 from typing import Any
 
 import tensorrt as trt
-import tensorrt_llm as trtllm
 import torch
+import torch.fx
 from loguru import logger
 from torch.fx import GraphModule
 from torch.fx.node import Node, Target
-from torch_tensorrt import dtype
-from torch_tensorrt._Input import Input
+from torch.fx.passes.shape_prop import TensorMetadata
+from torch_tensorrt import Input, dtype
 from torch_tensorrt.dynamo._engine_cache import BaseEngineCache
-from torch_tensorrt.dynamo._settings import CompilationSettings
 from torch_tensorrt.dynamo.conversion import (
     DYNAMO_CONVERTERS,
     CallingConvention,
@@ -21,15 +18,13 @@ from torch_tensorrt.dynamo.conversion import (
     UnsupportedOperatorException,
 )
 from torch_tensorrt.dynamo.conversion.converter_utils import create_constant
+from torch_tensorrt.logging import TRT_LOGGER
 
-from .config import DEFAULT_TRT_PROFILING_VERBOSITY
+from .configs import TensorRTBuilderConfig, TensorRTNetworkCreationFlags
 from .debug import (
-    EngineInfo,
     builder_config_as_dict,
-    get_dynamic_input_ranges,
-    get_human_readable_flags,
     open_debug_artifact,
-    save_onnx_without_weights,
+    save_for_debug,
 )
 from .fx.targets import GPTAttentionPlugin
 
@@ -39,30 +34,26 @@ class TRTLLMInterpreter(TRTInterpreter):
         self,
         module: GraphModule,
         input_specs: tuple[Input, ...],
-        logger_level: trt.ILogger.Severity = trt.ILogger.Severity.WARNING,
-        output_dtypes: tuple[dtype, ...] | None = None,
-        compilation_settings: CompilationSettings | None = None,
+        network_flags: TensorRTNetworkCreationFlags,
+        builder_config: TensorRTBuilderConfig,
         engine_cache: BaseEngineCache | None = None,
         network_name: str | None = None,
         output_names: list[str] | None = None,
-        strongly_typed: bool = True,
     ) -> None:
         super().__init__(
             module,
             input_specs=input_specs,
-            logger_level=logger_level,
-            output_dtypes=output_dtypes,
-            compilation_settings=compilation_settings or CompilationSettings(),
+            output_dtypes=infer_module_output_dtypes(module),
+            compilation_settings=builder_config.get_compilation_settings(),
             engine_cache=engine_cache,
         )
-
-        flags = 1 << trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH.value
-        if strongly_typed:
-            flags |= 1 << trt.NetworkDefinitionCreationFlag.STRONGLY_TYPED.value
-
-        self.ctx = ConversionContext(self.builder.create_network(flags), self.compilation_settings)
+        self.builder = trt.Builder(TRT_LOGGER)
+        self.ctx = ConversionContext(self.builder.create_network(network_flags.bitmask), self.compilation_settings)
+        self._builder_config = builder_config
+        if self.optimization_profiles:
+            self._builder_config.optimization_profiles = self.optimization_profiles
         self.logger = logger
-        self.placeholder_names = [n.name for n in module.graph.nodes if n.op == "placeholder"]
+        self.placeholder_names = [n.name for n in module.graph.find_nodes(op="placeholder")]
         self.user_output_names = output_names
         if network_name:
             self.ctx.net.name = network_name
@@ -70,82 +61,16 @@ class TRTLLMInterpreter(TRTInterpreter):
 
     def _construct_trt_network_def(self) -> None:
         super()._construct_trt_network_def()
-        with open_debug_artifact("trt_network_def.onnx", "wb") as f:
-            if f:
-                shape_ranges = (
-                    get_dynamic_input_ranges(self.ctx.net, profiles)
-                    if (profiles := self.optimization_profiles)
-                    else None
-                )
-                flags = get_human_readable_flags(self.ctx.net)
-                proto = EngineInfo.from_network_definition(self.ctx.net).as_onnx(shape_ranges, flags)
-                save_onnx_without_weights(proto, f)
+        save_for_debug("trt_network_def", self.ctx.net, self.optimization_profiles)
 
     def _populate_trt_builder_config(
         self,
         strict_type_constraints: bool = False,
         algorithm_selector: trt.IAlgorithmSelector | None = None,
         tactic_sources: int | None = None,
-        timing_cache: str | Path | trt.ITimingCache | None = None,
-        use_refit: bool = False,
-        profiling_verbosity: trt.ProfilingVerbosity = DEFAULT_TRT_PROFILING_VERBOSITY,
-        use_strip_plan: bool = False,
-        weight_streaming: bool = False,
-        weight_sparsity: bool = False,
     ) -> trt.IBuilderConfig:
-        assert (timing_cache is None) or (
-            algorithm_selector is None
-        ), "timing cache and algorithm selector cannot be specified at the same time"
-
-        builder_config = super()._populate_trt_builder_config(
-            # Workaround for the bug mentioned in https://github.com/pytorch/TensorRT/pull/3300
-            strict_type_constraints=False,
-            algorithm_selector=algorithm_selector,
-            tactic_sources=tactic_sources,
-        )
-
-        if strict_type_constraints:
-            builder_config.set_flag(trt.BuilderFlag.OBEY_PRECISION_CONSTRAINTS)
-
-        if weight_streaming:
-            builder_config.set_flag(trt.BuilderFlag.WEIGHT_STREAMING)
-
-        if use_refit:
-            builder_config.set_flag(trt.BuilderFlag.REFIT)
-
-        # Use fine-grained refit when strip plan is enabled in TRT10.2+.
-        if use_strip_plan:
-            builder_config.set_flag(trt.BuilderFlag.REFIT_INDIVIDUAL)
-
-        if use_strip_plan:
-            builder_config.set_flag(trt.BuilderFlag.STRIP_PLAN)
-
-        # Set TRT Engine profiling verbosity
-        builder_config.profiling_verbosity = profiling_verbosity
-
-        # set timing cache
-        cache = None
-        if timing_cache is not None:
-            # use given cache
-            if isinstance(timing_cache, trt.ITimingCache):
-                cache = timing_cache
-            # read cache from file
-            elif isinstance(timing_cache, str | Path) and os.path.exists(timing_cache):
-                with open(timing_cache, "rb") as f:
-                    cache = builder_config.create_timing_cache(f.read())
-            else:
-                trtllm.logger.warning("Invalid timing cache, using freshly created one")
-        if cache is None:
-            cache = builder_config.create_timing_cache(b"")
-        # When user does not given any existing cache, internally always created one
-        # so the cache should never None here
-        assert cache is not None and isinstance(cache, trt.ITimingCache)
-        builder_config.set_timing_cache(cache, ignore_mismatch=False)
-
-        # set weight sparsity
-        if weight_sparsity:
-            builder_config.set_flag(trt.BuilderFlag.SPARSE_WEIGHTS)
-
+        builder_config = self.builder.create_builder_config()
+        self._builder_config.copy_to(builder_config)
         with open_debug_artifact("builder_config.json") as f:
             if f:
                 json.dump(builder_config_as_dict(builder_config), f, indent=2, sort_keys=True)
@@ -217,3 +142,19 @@ def _format_output(output: Any) -> str:
         tokens = (f"{key}: {_format_output(x)}" for key, x in output.items())
         return f"[{','.join(tokens)}]"
     return f"{type(output).__name__}({output})"
+
+
+def infer_module_output_dtypes(
+    module: GraphModule,
+    truncate_double: bool = False,
+) -> tuple[dtype, ...]:
+    the_output = [n for n in module.graph.nodes if n.op == "output"][0]
+    output_dtypes: list[dtype] = []
+    for node in the_output.all_input_nodes:
+        if not isinstance((tensor_meta := node.meta.get("tensor_meta", None)), TensorMetadata):
+            raise RuntimeError(f"Found a graph output without tensor metadata: {node.format_node()}")
+        output_dtype = tensor_meta.dtype
+        output_dtypes.append(
+            dtype.float32 if truncate_double and output_dtype == torch.float64 else dtype._from(output_dtype)
+        )
+    return tuple(output_dtypes)
