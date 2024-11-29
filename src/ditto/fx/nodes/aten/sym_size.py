038e1816a0760d4e885b4dfa9906bf6989d75962
Jiwoong Choi <jiwoong.choi@squeezebits.com>
2024-11-29 17:22:43 +0900

    add: aten op node `SymSizeInt`

---
+++ src/ditto/fx/nodes/aten/sym_size.py
@@ -0,0 +1,26 @@
+# pyright: reportAttributeAccessIssue=false, reportReturnType=false, reportArgumentType=false
+
+import torch
+from torch.fx import Node
+
+from ...utils import get_tensor_metadata
+from .aten_op import ATenOp
+from .utils import make_dim_nonnegative
+
+
+@ATenOp.final(torch.ops.aten.sym_size.int)
+class SymSizeInt(ATenOp):
+    this: Node
+    dim: int
+
+    @property
+    def ndim(self) -> int | None:
+        if t := get_tensor_metadata(self.this):
+            return len(t.shape)
+        return None
+
+    @property
+    def nonnegative_dim(self) -> int | None:
+        if (ndim := self.ndim) is not None:
+            return make_dim_nonnegative(self.dim, ndim=ndim)
+        return None
