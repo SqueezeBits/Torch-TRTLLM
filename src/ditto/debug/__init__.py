69edc26e14c7465ac93ff09142fe75ca98ac95f6
Jiwoong Choi <jiwoong.choi@squeezebits.com>
2024-12-09 14:58:41 +0900

    add: `should_save_debug_artifacts`

--- src/ditto/debug/__init__.py
+++ src/ditto/debug/__init__.py
@@ -1,4 +1,10 @@
 from .engine import EngineInfo
 from .fx import build_onnx_from_fx
-from .io import get_debug_artifacts_dir, open_debug_artifact, save_for_debug, save_onnx_without_weights
+from .io import (
+    get_debug_artifacts_dir,
+    open_debug_artifact,
+    save_for_debug,
+    save_onnx_without_weights,
+    should_save_debug_artifacts,
+)
 from .network import builder_config_as_dict, get_human_readable_flags
