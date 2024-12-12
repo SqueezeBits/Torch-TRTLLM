c2424bffa5d7d2eb47579cba74280882f15e3cc3
Jiwoong Choi <jiwoong.choi@squeezebits.com>
2024-12-12 18:23:47 +0900

    chore: import literal types for trtllm configs

--- src/ditto/configs/trtllm/__init__.py
+++ src/ditto/configs/trtllm/__init__.py
@@ -1,5 +1,12 @@
 from .build import TRTLLMBuildConfig
 from .engine import TRTLLMEngineConfig
+from .literals import (
+    DTypeLiteral,
+    KVCacheTypeLiteral,
+    LoraCheckpointLiteral,
+    PluginFlag,
+    QuantAlgoLiteral,
+)
 from .lora import TRTLLMLoraConfig
 from .model import TRTLLMModelConfig
 from .optimization_profile import TRTLLMOptimizationProfileConfig
