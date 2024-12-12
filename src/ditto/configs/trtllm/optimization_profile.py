40e11463b4910bb00df4fcb743f82f90972f94ad
Jiwoong Choi <jiwoong.choi@squeezebits.com>
2024-12-12 18:26:12 +0900

    feat: create optimization profile config from HF config and plugin config

--- src/ditto/configs/trtllm/optimization_profile.py
+++ src/ditto/configs/trtllm/optimization_profile.py
@@ -1,17 +1,22 @@
+import math
 from typing import Any
 
 from loguru import logger
 from pydantic import Field, PrivateAttr, computed_field, model_validator
+from transformers import PretrainedConfig
 from typing_extensions import Self
 
 from ...types import StrictlyTyped
+from .plugin import TRTLLMPluginConfig
+
+DEFAULT_MAX_POS_EMBEDDING: int = 4096
 
 
 class RuntimeTRTLLMOptimizationProfileConfig(StrictlyTyped):
     """A subset of properties in `trtllm.BuildConfig` related to optimization profile required at runtime."""
 
     max_input_len: int = Field(default=1024, gt=1)
-    max_seq_len: int = Field(default=4096, gt=1)
+    max_seq_len: int = Field(default=DEFAULT_MAX_POS_EMBEDDING, gt=1)
     max_batch_size: int = Field(default=256, gt=1)
     max_beam_width: int = Field(default=1, gt=0)
     max_num_tokens: int = Field(default=8192, multiple_of=8, gt=1)
@@ -22,9 +27,7 @@ class RuntimeTRTLLMOptimizationProfileConfig(StrictlyTyped):
     @property
     def opt_num_tokens(self) -> int:
         if self._opt_num_tokens is None:
-            opt_num_tokens = min(
-                self.max_num_tokens, self.max_batch_size * self.max_beam_width
-            )
+            opt_num_tokens = min(self.max_num_tokens, self.max_batch_size * self.max_beam_width)
             self._opt_num_tokens = 8 * max(int(round(opt_num_tokens / 8)), 1)
         return self._opt_num_tokens
 
@@ -71,9 +74,7 @@ class RuntimeTRTLLMOptimizationProfileConfig(StrictlyTyped):
         assert (
             self.opt_batch_size <= self.max_batch_size
         ), f"{self.opt_batch_size=} must be at most {self.max_batch_size=}."
-        assert (
-            self._opt_num_tokens is None or self._opt_num_tokens <= self.max_num_tokens
-        )
+        assert self._opt_num_tokens is None or self._opt_num_tokens <= self.max_num_tokens
         if self.max_num_tokens > (upper_bound := 16384):
             logger.warning(
                 f"Specifying a {self.max_num_tokens=} larger than {upper_bound} is usually not recommended. You might "
@@ -89,8 +90,19 @@ class TRTLLMOptimizationProfileConfig(RuntimeTRTLLMOptimizationProfileConfig):
     opt_beam_width: int = Field(default=1, gt=0)
     opt_kv_cache_block_size: int = Field(default=32, gt=0)
     max_kv_cache_block_size: int = Field(default=64, gt=1)
-    opt_attention_window_size: int = Field(default=2048, gt=0)
-    max_attention_window_size: int = Field(default=4096, gt=1)
+    opt_attention_window_size: int = Field(default=DEFAULT_MAX_POS_EMBEDDING // 2, gt=0)
+    max_attention_window_size: int = Field(default=DEFAULT_MAX_POS_EMBEDDING, gt=1)
+
+    @classmethod
+    def create_from(cls, hf_config: PretrainedConfig, plugin_config: TRTLLMPluginConfig) -> Self:
+        max_position_embeddings = getattr(hf_config, "max_position_embeddings", DEFAULT_MAX_POS_EMBEDDING)
+        max_kv_cache_block_size = math.ceil(max_position_embeddings / plugin_config.tokens_per_block)
+        return cls(
+            max_seq_len=max_position_embeddings,
+            max_attention_window_size=max_position_embeddings,
+            max_kv_cache_block_size=max_kv_cache_block_size,
+            opt_kv_cache_block_size=max_kv_cache_block_size // 2,
+        )
 
     def runtime(self) -> RuntimeTRTLLMOptimizationProfileConfig:
         runtime_config = RuntimeTRTLLMOptimizationProfileConfig(
@@ -111,12 +123,8 @@ class TRTLLMOptimizationProfileConfig(RuntimeTRTLLMOptimizationProfileConfig):
         The criterions and error messages are adopted from `tensorrt_llm._common.check_max_num_tokens`.
         While TRT-LLM tries to adjust the wrong values provided by the user, we will simply reject them.
         """
-        assert (
-            self.opt_seq_len <= self.max_seq_len
-        ), f"{self.opt_seq_len=} must be at most {self.max_seq_len=}."
-        assert (
-            self.opt_beam_width <= self.max_beam_width
-        ), f"{self.opt_seq_len=} must be at most {self.max_seq_len=}."
+        assert self.opt_seq_len <= self.max_seq_len, f"{self.opt_seq_len=} must be at most {self.max_seq_len=}."
+        assert self.opt_beam_width <= self.max_beam_width, f"{self.opt_seq_len=} must be at most {self.max_seq_len=}."
         assert (
             self.opt_kv_cache_block_size <= self.max_kv_cache_block_size
         ), f"{self.opt_kv_cache_block_size=} must be at most {self.max_kv_cache_block_size=}."
