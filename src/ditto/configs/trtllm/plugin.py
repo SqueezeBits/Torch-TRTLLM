ab51b893c7f0bdae65965781d1c66ed0e2bf9cb8
Jiwoong Choi <jiwoong.choi@squeezebits.com>
2024-12-12 18:27:13 +0900

    feat: create plugin config from model dtype

--- src/ditto/configs/trtllm/plugin.py
+++ src/ditto/configs/trtllm/plugin.py
@@ -1,3 +1,7 @@
+import torch
+from loguru import logger
+from typing_extensions import Self
+
 from ...types import StrictlyTyped
 from .literals import PluginFlag
 
@@ -13,3 +17,23 @@ class TRTLLMPluginConfig(StrictlyTyped):
     tokens_per_block: int = 64
     use_paged_context_fmha: bool = False
     paged_state: bool = False
+
+    @classmethod
+    def create_from(cls, dtype: torch.dtype) -> Self:
+        plugin_flag = get_plugin_flag(dtype)
+        return cls(
+            gpt_attention_plugin=plugin_flag,
+            gemm_plugin=plugin_flag,
+        )
+
+
+def get_plugin_flag(dtype: torch.dtype) -> PluginFlag:
+    mapping: dict[torch.dtype, PluginFlag] = {
+        torch.float16: "float16",
+        torch.bfloat16: "bfloat16",
+        torch.float8_e4m3fn: "fp8",
+    }
+    if dtype not in mapping:
+        logger.warning(f"{dtype} is not supported for TRT-LLM plugins. Will use the plugin flag 'auto' instead.")
+        return "auto"
+    return mapping[dtype]
