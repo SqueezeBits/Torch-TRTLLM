6a241d37bed74250ee325a75f6879ca7c9085f21
Jiwoong Choi <jiwoong.choi@squeezebits.com>
2024-12-16 13:01:19 +0900

    refactor: use `get_memory_footprint`

--- src/ditto/fx/passes/graph_pass.py
+++ src/ditto/fx/passes/graph_pass.py
@@ -1,10 +1,9 @@
-import torch
 from loguru import logger
 from torch.fx import GraphModule
 from torch.fx.passes.infra.pass_base import PassBase, PassResult
 from torch_tensorrt.dynamo.lowering.passes.pass_utils import clean_up_graph_after_modifications
 
-from ...constants import DEFAULT_DEVICE
+from ...debug import get_memory_footprint
 
 
 class GraphOptimizationPass(PassBase):
@@ -27,20 +26,5 @@ class GraphOptimizationPass(PassBase):
         else:
             logger.debug(f"{self.indent}-> no result returned")
         self.ensures(graph_module)
-        log_memory_footprint(graph_module)
+        logger.opt(lazy=True).trace("Memory Footprint: {m}", m=get_memory_footprint)
         return result
-
-
-def log_memory_footprint(graph_module: GraphModule) -> None:
-    try:
-        device = next(iter(graph_module.parameters())).device
-    except StopIteration:
-        device = torch.device(DEFAULT_DEVICE)
-
-    logger.opt(lazy=True).trace(
-        "Allocated Memory: {x:.2f} MB", x=lambda: torch.cuda.memory_allocated(device) / 1024**2
-    )
-    logger.opt(lazy=True).trace(
-        "Max Allocated Memory: {x:.2f} MB", x=lambda: torch.cuda.max_memory_allocated(device) / 1024**2
-    )
-    logger.opt(lazy=True).trace("Cached Memory: {x:.2f} MB", x=lambda: torch.cuda.memory_reserved(device) / 1024**2)
