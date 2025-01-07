ec24b8e732d3e8be21e252f59e922eb5d60e326a
Jiwoong Choi <jiwoong.choi@squeezebits.com>
2025-01-07 19:17:21 +0900

    refactor: make `are_consecutive` as a classmethod of `Slice`

--- src/ditto/fx/nodes/aten/slice.py
+++ src/ditto/fx/nodes/aten/slice.py
@@ -2,11 +2,12 @@
 
 import torch
 from torch.fx.node import Node
+from typing_extensions import Self
 
 from ....types import SymbolicInteger
 from ...utils import get_tensor_metadata
 from .aten_op import ATenOp, FinalATenOp
-from .utils import make_axis_nonnegative, make_dim_nonnegative
+from .utils import has_same_values, make_axis_nonnegative, make_dim_nonnegative
 
 
 @ATenOp.register(torch.ops.aten.slice.Tensor)
@@ -16,6 +17,23 @@ class Slice(FinalATenOp):
     start: SymbolicInteger | Node | None = None
     end: SymbolicInteger | Node | None = None
     step: SymbolicInteger | Node = 1
+
+    @classmethod
+    def are_consecutive(cls, slice_nodes: list[Self]) -> bool:
+        if not all(isinstance(s.start, int) for s in slice_nodes):
+            slice_nodes = sorted(slice_nodes, key=lambda s: start if isinstance(start := s.start, int) else 0)
+
+        return (
+            len({s.this for s in slice_nodes}) == 1
+            and len(dim_sizes := {s.dim_size for s in slice_nodes}) == 1
+            and None not in dim_sizes
+            and all(s.step == 1 for s in slice_nodes)
+            and all(
+                has_same_values(slice_nodes[i].nonnegative_end, slice_nodes[i + 1].nonnegative_start)
+                for i in range(len(slice_nodes) - 1)
+            )
+            and has_same_values(slice_nodes[-1].nonnegative_end, slice_nodes[0].dim_size)
+        )
 
     @property
     def dim_size(self) -> int | None:
--- src/ditto/fx/passes/fuse_consecutive_slice_concat.py
+++ src/ditto/fx/passes/fuse_consecutive_slice_concat.py
@@ -1,6 +1,7 @@
 from torch.fx import Node
 
 from ..nodes import Cat, Slice
+from ..nodes.aten.utils import has_same_values
 from .infra import NodewiseOptimizationPass, NodewisePassResult, ReplaceAllUses
 
 
@@ -13,27 +14,7 @@ class FuseConsecutiveSliceConcat(NodewiseOptimizationPass):
             and (slice_nodes := [s for x in cat_node.tensors if (s := Slice.specialize_from(x))])
             and len(slice_nodes) == len(cat_node.tensors)
             and has_same_values(slice_nodes[0].nonnegative_dim, cat_node.nonnegative_dim)
-            and are_consecutive(slice_nodes)
+            and Slice.are_consecutive(slice_nodes)
         ):
             return {}
         return {cat_node.node: ReplaceAllUses(by=slice_nodes[0].this)}
-
-
-def are_consecutive(slice_nodes: list[Slice]) -> bool:
-    return (
-        len({s.this for s in slice_nodes}) == 1
-        and len(dim_sizes := {s.dim_size for s in slice_nodes}) == 1
-        and None not in dim_sizes
-        and all(s.step == 1 for s in slice_nodes)
-        and all(
-            has_same_values(slice_nodes[i].nonnegative_end, slice_nodes[i + 1].nonnegative_start)
-            for i in range(len(slice_nodes) - 1)
-        )
-        and has_same_values(slice_nodes[-1].nonnegative_end, slice_nodes[0].dim_size)
-    )
-
-
-def has_same_values(x: int | None, y: int | None) -> bool:
-    if x is None or y is None:
-        return False
-    return x == y
