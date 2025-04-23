13c9d0c3ee5a176b3723f2e13219d7d219beddb5
changjun.lee <changjun.lee@squeezebits.com>
2025-04-23 21:22:23 +0900

    fix: wrong operation name

--- src/ditto/patches/modelopt.py
+++ src/ditto/patches/modelopt.py
@@ -15,7 +15,7 @@
 import torch
 from modelopt.torch.quantization.nn.modules.quant_module import QuantLinearConvBase
 
-from ..custom_ops import ditto_dequantize
+from ..custom_ops import ditto_fake_quantize
 from .patch import custom_patch
 
 
@@ -28,7 +28,7 @@ from .patch import custom_patch
 def patch_quantize_forward() -> None:
     def patched_forward(self: QuantLinearConvBase, inp: torch.Tensor, *args, **kwargs) -> torch.Tensor:
         if self.input_quantizer.is_enabled:
-            inp = ditto_dequantize(
+            inp = ditto_fake_quantize(
                 inp,
                 8,
                 self.input_quantizer._dynamic,
@@ -36,7 +36,7 @@ def patch_quantize_forward() -> None:
                 self.input_scale if self.input_quantizer._dynamic is False else None,
             )
         if self.weight_quantizer.is_enabled:
-            weight = ditto_dequantize(
+            weight = ditto_fake_quantize(
                 self.unpacked_weight,
                 8 if self.weight_quantizer.num_bits == (4, 3) else self.weight_quantizer.num_bits,
                 self.weight_quantizer._dynamic,
@@ -48,6 +48,16 @@ def patch_quantize_forward() -> None:
         else:
             weight = self.weight
         out = torch.nn.functional.linear(inp, weight, self.bias if self.bias is not None else None)
+
+        if self.output_quantizer.is_enabled:
+            out = ditto_fake_quantize(
+                out,
+                8 if self.output_quantizer.num_bits == (4, 3) else self.output_quantizer.num_bits,
+                self.output_quantizer._dynamic,
+                out.dtype,
+                self.output_scale if self.output_quantizer._dynamic is False else None,
+            )
+
         return out
 
     QuantLinearConvBase.forward = patched_forward
